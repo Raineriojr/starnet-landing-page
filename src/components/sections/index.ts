022ff95e51d0a5cfb43a246a73f69857c8b7b37e
Raineriojr <raijunior20@gmail.com>
2026-02-23 12:16:12 -0300

    fix: remove import

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,5 +1,4 @@
 export { Hero } from "./Hero";
 export { About } from "./About";
 export { Services } from "./Services";
-export { Testimonials } from "./Testimonials";
 export { CTA } from "./CTA";
